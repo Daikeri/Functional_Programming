c8a37f439c9c54633c1745247f1155c802c0fc6b
Daikeri <kotdmitrii65@gmail.com>
2022-04-11 11:29:05 +0300

    14-я
    сделаль

---
+++ LR_5/Number_14/Program.fs
@@ -0,0 +1,21 @@
+﻿open System
+
+let Div value func init =
+    let rec DivR value func init acc =
+        if acc = 0 then init
+        else
+            let prom= if value%acc=0 then func acc init else init
+            let newd=acc-1
+            DivR value func prom newd
+    DivR value func init value
+    
+    
+[<EntryPoint>]
+  let main argv =
+    printfn "intput the number"
+    let value = Console.ReadLine() |> Int32.Parse
+    let result = Div value (fun z k -> z+k) 0
+    result |> printfn "%d"
+    0
+
+     
