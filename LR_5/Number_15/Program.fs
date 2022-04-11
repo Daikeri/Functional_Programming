3c2fe7ddcea52fafdd5f0c13ff43e6584bb72a50
Daikeri <kotdmitrii65@gmail.com>
2022-04-11 22:44:03 +0300

    туту
    исправил 15-е и добавил 16-е

--- LR_5/Number_15/Program.fs
+++ LR_5/Number_15/Program.fs
@@ -1,12 +1,30 @@
-﻿// Learn more about F# at http://docs.microsoft.com/dotnet/fsharp
-// See the 'F# Tutorial' project for more help.
-
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+﻿open System
+let rec FindGCD one two =
+    if one=0 || two = 0 then one + two
+    else 
+        let none =
+            if one > two then one % two
+            else one 
+        let ntwo =
+            if one <= two then two % one
+            else two
+        FindGCD none ntwo
+        
+let PrimeNumber value func init =
+    let rec PrimeNumberR value func init acc =
+        if acc <= 0 then init
+        else 
+            let ninit = if FindGCD value acc = 1 then func value init else init
+            let nacc = acc - 1
+            PrimeNumberR value func ninit nacc
+    PrimeNumberR value func init value
+    
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
-    0 // return an integer exit code
+    printfn "input the number"
+    let x = Console.ReadLine() |> Int32.Parse
+    let result = PrimeNumber x (fun x y -> x + y) 0
+    result |> printfn "%d"
+    0 
+    
