2b8e36d91262ded7b9353e64febc63f7ee67e47f
Daikeri <kotdmitrii65@gmail.com>
2022-04-19 13:41:28 +0300

    31-я

---
+++ LR_6/Number_16.31/Program.fs
@@ -0,0 +1,20 @@
+﻿open System
+let even list =
+   let rec prom list quan =
+       match list with
+       |[] -> quan
+       |head::tail -> 
+           let newq = if head % 2 = 0 then quan+1 else quan
+           prom tail newq
+   prom list 0
+let f31 list =
+    match list with
+    |[] -> 0
+    |_ -> even list
+
+[<EntryPoint>]
+    let main argv =
+        let l = [1..10]
+        let result = f31 l
+        printfn "%O" result // 5
+        0
