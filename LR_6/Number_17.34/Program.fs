b1a32931d37ff4fbc4deaad4c7bf76abaed89aa9
Daikeri <kotdmitrii65@gmail.com>
2022-04-20 11:03:52 +0300

    34-я

---
+++ LR_6/Number_17.34/Program.fs
@@ -0,0 +1,25 @@
+﻿open System
+(*1.34 Дан целочисленный массив и отрезок a..b. Необходимо найти
+элементы, значение которых принадлежит этому отрезку.*)
+let segment list (a,b) =
+    let rec prom list (a,b) acc =
+        match list with
+        |[] -> acc
+        |head::tail ->
+            let nacc = if head >= a && head <= b then head::acc else acc
+            prom tail (a,b) nacc
+    prom list (a,b) []
+
+let rec printl =
+    function
+    |[] -> ()
+    |head::tail ->
+        printfn "%O" head
+        printl tail
+
+[<EntryPoint>]
+let main argv =
+    let l = [1;2;3;12;17;80;-8;20;10;5]
+    let ab = (10,20)
+    segment l ab |> printl 
+    0 
