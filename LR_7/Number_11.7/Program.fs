87de83fd61c1e181f0bd67545d27e13661b1daaa
Daikeri <kotdmitrii65@gmail.com>
2022-04-29 02:02:51 +0300

    7-я оч крута получилось

---
+++ LR_7/Number_11.7/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let shift_right list =
+    list |> List.permute (fun x ->
+    if x > ((List.length list-1)-2) then (x+1)%(List.length list-1)
+    else x+2) |> printfn "%A"
+
+[<EntryPoint>]
+let main argv =
+
+    let l = [1..4]
+    let k = [1;7;3;45;-1;234;56;-99]
+    shift_right l
+    shift_right k
+    0
