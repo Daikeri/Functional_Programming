06d3de6bc29a7c71574ccf98c7de875a1cd8cf14
Daikeri <kotdmitrii65@gmail.com>
2022-04-29 15:52:52 +0300

    27-я

---
+++ LR_7/Number_13.27/Program.fs
@@ -0,0 +1,15 @@
+﻿open System
+
+let shift_left list =
+    list |> List.permute (fun x ->
+    if x=0 then x+(List.length list-1)
+    else x-1) |> printfn "%A"
+
+[<EntryPoint>]
+let main argv =
+    
+    let l = [1..4]
+    let k = [1;7;3;45;-1;234;56;-99]
+    shift_left l
+    shift_left k
+    0 
