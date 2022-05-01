edf51ffe0c4229d870bb066105ec96c32d0968b1
Daikeri <kotdmitrii65@gmail.com>
2022-05-01 16:48:28 +0300

    пофиксил теперь крута

--- LR_7/Number_16.57/Program.fs
+++ LR_7/Number_16.57/Program.fs
@@ -1,13 +1,13 @@
 ﻿open System
 
-let more_than list = 
-    list |> List.filter (fun x ->
-    x > (list |> List.take(list |> List.findIndex (fun y -> y = x)) |> List.sum) && x <> list.Head ) |> printfn "%A" 
+let more_than_2 list = 
+    list |> List.indexed |> List.filter (fun x ->
+    (snd x) > ((0,(list |> List.take (fst x)) ) ||> List.fold (fun acc k -> acc + k)) && snd x <> list.Head ) |> printfn "%A" 
 
 [<EntryPoint>]
 let main argv =
     let l = [1;2;3;10;5;5;70] 
-    let k = [100;100;100;400;2000;1000;-1000;4000;-99;-1;34;55000]
-    more_than l
-    more_than k
+    let k = [100;100;100;400;1000;1000;-1000;4000;-99;-1;34;55000]
+    more_than_2 l
+    more_than_2 k
     0
