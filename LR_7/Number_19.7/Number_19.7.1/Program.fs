128146d84e7196f55318f4716548992864fa72df
Daikeri <kotdmitrii65@gmail.com>
2022-05-03 22:31:03 +0300

    хорошая

---
+++ LR_7/Number_19.7/Number_19.7.1/Program.fs
@@ -0,0 +1,10 @@
+﻿open System
+
+let quan_rus s =
+    s |> String.filter (fun k -> k >= 'А' && k <= 'я') |> String.length |> printf "%d"
+
+[<EntryPoint>]
+let main argv =
+    let inp = Console.ReadLine()
+    quan_rus inp
+    0
