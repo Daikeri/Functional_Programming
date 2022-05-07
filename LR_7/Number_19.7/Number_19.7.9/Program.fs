4f2bbb7d08e6355789fdb87d21e2a65615d45052
Daikeri <kotdmitrii65@gmail.com>
2022-05-07 19:22:33 +0300

    смак

---
+++ LR_7/Number_19.7/Number_19.7.9/Program.fs
@@ -0,0 +1,13 @@
+﻿open System
+(*
+Дана строка. Необходимо проверить образуют ли строчные
+символы латиницы палиндром.
+*)
+let palindrom (strr:string) =
+    strr.Split(' ') |> Array.filter (fun x -> x = (String.Concat<char>(x.ToCharArray() |> Array.rev))) |> printfn "%A"
+    
+[<EntryPoint>]
+let main argv =
+    let str = "gig kayak horse level madam input qwerty fish "
+    palindrom str
+    0
